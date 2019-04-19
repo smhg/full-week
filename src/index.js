ea414b049b862903d0d1a81456e1fcc06a14b74d
smhg <sam.hauglustaine@gmail.com>
2019-04-19 15:44:45 +0200

    return full weeks related to end of month/year (closes #2)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
+const isoWeekday = (date = new Date()) => (date.getDay() + 6) % 7 + 1;
+
 export default function fullWeek (idx = 1, year, month = 0) {
-  const isoWeekday = ((8 - (new Date(year, month, 1)).getDay()) % 7) + 1;
+  const weekday = isoWeekday(new Date(year, month, 1));
+
+  if (idx < 0) {
+    return new Date(
+      year,
+      month,
+      7 * idx - weekday + 2
+    );
+  }
 
   return new Date(
     year,
     month,
-    isoWeekday + 7 * (idx - 1)
+    7 * idx + (8 - weekday) % 7 - 6
   );
 }
--- test/index.js
+++ test/index.js
@@ -10,11 +10,28 @@ describe('fullWeek', () => {
     assert.deepStrictEqual(fullWeek(2, 2018), new Date(2018, 0, 8));
   });
 
+  it('should return start of last full week of previous year', () => {
+    assert.deepStrictEqual(fullWeek(-1, 2018), new Date(2017, 11, 25));
+  });
+
+  it('should return start of third to last full week of previous year', () => {
+    assert.deepStrictEqual(fullWeek(-3, 2019), new Date(2018, 11, 10));
+  });
+
   it('should return start of first full week of month', () => {
+    assert.deepStrictEqual(fullWeek(1, 2018, 0), new Date(2018, 0, 1));
     assert.deepStrictEqual(fullWeek(1, 2018, 7), new Date(2018, 7, 6));
   });
 
   it('should return start of second full week of month', () => {
     assert.deepStrictEqual(fullWeek(2, 2018, 7), new Date(2018, 7, 13));
+  });
+
+  it('should return start of last full week of previous month', () => {
+    assert.deepStrictEqual(fullWeek(-1, 2018, 8), new Date(2018, 7, 20));
+  });
+
+  it('should return start of second to last full week of previous month', () => {
+    assert.deepStrictEqual(fullWeek(-2, 2018, 8), new Date(2018, 7, 13));
   });
 });
